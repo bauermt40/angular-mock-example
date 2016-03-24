7df9815178bdd14d7d3c21f177a3bd3ac1e0eac4
bauermt40 <matthewbauer@gmail.com>
2016-03-23 22:03:34 -0400

    Adding $httpBackend mock

--- spec/omdb/service.spec.js
+++ spec/omdb/service.spec.js
@@ -1,13 +1,29 @@
 describe('omdb service', function() {
-   it('should return search movie data', function() {
-        var omniApi = {};
+    var movieData = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.9","imdbVotes":"342","imdbID":"tt0251413","Type":"game","Response":"True"};
+    
+    var omdbApi = {};
+    var $httpBackend;
+
+    beforeEach(module('omni'));
+
+    beforeEach(inject(function(_omdbApi_, _$httpBackend_) {
+        omdbApi = _omdbApi_;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    it('should return search movie data', function() {
+        var response;
         
-        angular.mock.module('omni');
+        $httpBackend.when('GET', 'http://www.omdbapi.com/v=1&s=star%20wars')
+            .responsd(200, movieData);
         
-        angular.mock.inject(function(_omniApi_) {
-            omniApi = _omniApi_;
+        omdbApi.search('star wars')
+            .then(function(data) {
+            response = data;
         });
         
-        expect(service.search('star wars')).toBe(movieData);
+        $httpBackend.flush();
+        
+        expect(response).toEqual(movieData);
     });
 });
--- src/omdb/service.js
+++ src/omdb/service.js
@@ -1,8 +1,19 @@
-angular.module('omni', [])
-    .factory('omniApi', function() {
+angular.module('omdb', [])
+    .factory('omdbApi', function($http, $q) {
         var service = {};
-    
+        var baseUrl = 'http://www.omdbapi.com/v=1&';
         service.search = function(query) {
-            return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-   Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.9","imdbVotes":"342","imdbID":"tt0251413","Type":"game","Response":"True"};
+            var deferred = $q.defer();
+            
+            $http.get(baseUrl + 's=' + encodeURIComponent(query))
+            .then(function(response) {
+                deferred.resolve(response);
+            }, function(err) {
+                console.log(err);
+            });
+            
+            return deferred.promise;
         }
+        
+        return service;
     });
